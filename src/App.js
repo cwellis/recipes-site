249c6eef26a60fa39d2ff28846a151e19e1b5b19
cwellis <cwellis2016@gmail.com>
2022-06-20 20:44:14 -0400

    added links to the home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import './index.css';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+// components/ pages
+import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+
+      <BrowserRouter>
+
+        <nav>
+          <h1>Recipe Website</h1>
+          
+          <header>
+            <Link className='link' to='/'>Home</Link>
+            <Link to='/Addrecipe'>Add Recipe</Link>
+          </header>
+          
+    
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          {/* <Route path='/Addrecipe' element={<Addrecipe />} /> */}
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
