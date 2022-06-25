c25d228b9fab71ad2e2440b17ce91e389006508d
Gideon Girigiri <gideongirigiri@gamil.com>
2022-06-24 23:34:18 -0400

    added logic to send data to backend, hopefully this help you Casey

--- src/components/Addrecipe.jsx
+++ src/components/Addrecipe.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 let Addrecipe = () => {
 
+    const saveRecipe = (event) => {
+        fetch("http://localhost:5000/createrecipe", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"hello": event.target.value})
+        })
+    }
+
     return (
         <div className='app'>
-            
+
             <textarea 
                 autoFocus
                 className='textarea'
+                onChange={saveRecipe}
             />
 
         </div>
