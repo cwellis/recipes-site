b6db38d85d55e9a82c3e8bd0a80ccfe7aa0ff855
cwellis <cwellis2016@gmail.com>
2022-07-05 20:14:58 -0400

    fixed sign in form missing

---
+++ backend/models/recipeModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const recipeSchema = new Schema({
+    markDown: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Recipe', recipeSchema)
