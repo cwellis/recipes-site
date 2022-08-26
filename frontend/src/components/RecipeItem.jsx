38926bc4aa0ceb04bcf435101220505fee2efd5a
cwellis <cwellis2016@gmail.com>
2022-08-26 10:19:48 -0400

    added home page

--- frontend/src/components/RecipeItem.jsx
+++ frontend/src/components/RecipeItem.jsx
@@ -4,6 +4,10 @@ import { deleteRecipe } from '../features/recipes/recipeSlice'
 function RecipeItem({ recipe }) {
   const dispatch = useDispatch()
 
+  const handleDelete = () => {
+    window.confirm("Delete?") ? dispatch(deleteRecipe(recipe._id)) : console.log('')
+  }
+
   return (
     <div className='recipe'>
       <div>{new Date(recipe.createdAt).toLocaleString('en-US')}</div>
@@ -12,10 +16,11 @@ function RecipeItem({ recipe }) {
       <h2>Cook Time: {recipe.cookTime}</h2>
       <h2>Ingredients: {recipe.ingredients}</h2>
       <h2>Instructions: {recipe.instructions}</h2>
-      <button onClick={() => {
-        dispatch(deleteRecipe(recipe._id))
-      }
-      } className='close'>
+
+      <button 
+
+        onClick={handleDelete}
+        className='close'>
         Delete Recipe
       </button>
     </div>
