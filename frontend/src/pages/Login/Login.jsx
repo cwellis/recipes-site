338395d901093a15c555b4cf44f0d9ccb9e6d4cd
cwellis <cwellis2016@gmail.com>
2022-11-08 22:57:47 -0500

    login and register screen css

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { login, reset } from '../../features/auth/authSlice'
+import './Login.css'
 
 function Login() {
     
@@ -60,51 +61,65 @@ function Login() {
     }
 
     return (
-        <>
-            <section className="heading">
-                <h1>
-                    <FaSignInAlt /> Login
-                </h1>
-                <p>
-                    Login and Start Setting Recipes
-                </p>
-            </section>
+        <div className='login-wrapper'>
 
-            <section className="form">
-                <form onSubmit={onSubmit}>
+            <div className="left-wrapper">
+                Recipe Site
+            </div>
 
-                    <div className="form-group">
-                        <input 
-                            type="text" 
-                            id='email' 
-                            name='email' 
-                            value={email} 
-                            placeholder='Enter Email' 
-                            onChange={onChange} 
-                            className='form-control' 
-                        />
-                    </div>
+            <div className='right-wrapper'>
 
-                    <div className="form-group buttonIn">
-                        <input 
-                            id='password' 
-                            name='password' 
-                            value={password} 
-                            placeholder='Enter Password' 
-                            onChange={onChange} 
-                            type={visible ? "password" : "text"} className='form-control'
-                        />
-                        <button type='button' className='viewBtn' onClick={handleVis}>
-                            {visible ? "View" : "Hide"}
-                        </button>
-                    </div>
-                    
-                    <div className="form-group">
-                        <button type="submit" className='btn btn-block'>Submit</button>
-                    </div>
-                </form>
-            </section>
-        </>
+                <div className='login-container'>
+
+                    <section className="login-heading">
+                        <div>
+                            <h1>
+                                <FaSignInAlt /> Login
+                            </h1>
+                            <p>
+                                Login To Your Account
+                            </p>
+                        </div>
+                    </section>
+
+                    <section className="login-form">
+                        <form onSubmit={onSubmit}>
+
+                            <div className="form-group">
+                                <input 
+                                    type="text" 
+                                    id='email' 
+                                    name='email' 
+                                    value={email} 
+                                    placeholder='Enter Email' 
+                                    onChange={onChange} 
+                                    className='form-control' 
+                                    />
+                            </div>
+
+                            <div className="form-group buttonIn">
+                                <input 
+                                    id='password' 
+                                    name='password' 
+                                    value={password} 
+                                    placeholder='Enter Password' 
+                                    onChange={onChange} 
+                                    type={visible ? "password" : "text"} className='form-control'
+                                    />
+                                <button type='button' className='viewBtn' onClick={handleVis}>
+                                    {visible ? "View" : "Hide"}
+                                </button>
+                            </div>
+                            
+                            <div className="form-group">
+                                <button type="submit" className='btn btn-block'>Submit</button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
